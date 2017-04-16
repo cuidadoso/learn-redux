c59adfcbbf6b943508f9a5b3b0cfb65cbeb308b7
cuidadoso <alexander_s@front.ru>
2017-04-16 17:01:21 +0200

    19. Learning Redux Dev Tools.

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -2,7 +2,7 @@
  * Created by apyreev on 14-Apr-17.
  */
 // external dependencies
-import { createStore, compse } from 'redux';
+import { createStore, compose } from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 // import the root reducer
@@ -15,8 +15,13 @@ const defaultState = {
   comments,
   posts
 };
+// To use plugin ReduxDevTools in browser(Chrome)
+const enhancers = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() ?
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+);
 
-const store = createStore(rootReducer, defaultState);
+const store = createStore(rootReducer, defaultState, enhancers);
 
 export const history = syncHistoryWithStore(browserHistory, store);
 // TODO integration hot reloading with WebStorm.
